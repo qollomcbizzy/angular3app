bd7ead8346ccf809716226f5c15fdf6c6abfdc4f
collo <qollomcbizzy64@gmail.com>
2017-09-11 13:44:45 +0300

    corrected the @import word with @Input keyword and added its import

--- app/Task-list.component.ts
+++ app/Task-list.component.ts
@@ -1,16 +1,16 @@
-import {Component} from '@angular/core';
+import {Component,Input} from '@angular/core';
 import {Task} from './task.model';
 @Component
 ({
   selector:'task-list',
   template:`
    <div class="row">
-   <div class="tasked" *ngFor="let currentTask of tasks">
+   <div class="tasked" *ngFor="let currentTask of childTaskList">
    <h3>{{currentTask.description}}</h3>
    <button class="btn btn-primary" (click)="showDetails(currentTask)">Edit</button>
    </div>
    `
 })
 export class TaskListComponent{
-  @import () childTaskList :Task[];
+  @Input() childTaskList :Task[];
 }
