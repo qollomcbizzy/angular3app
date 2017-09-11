54bbc449526f06bfcaacf89df5f267bab9f7ed12
collo <qollomcbizzy64@gmail.com>
2017-09-11 12:59:42 +0300

    made changes to the task-list.component.ts and added the bower dependencies and inittialized the npm

--- app/Task-list.component.ts
+++ app/Task-list.component.ts
@@ -1,11 +1,25 @@
 import {Component} from '@angular/core';
 @Component
 ({
-  selector:'Task-list',
+  selector:'task-list',
   template:`
-
+   <div class="row">
+   <div class="tasked" *ngFor="let currentTask of tasks">
+   <h3>{{currentTask.description}}</h3>
+   <button class="btn btn-primary" (click)="showDetails(currentTask)">Edit</button>
+   </div>
    `
 })
 export class TaskListComponent{
-
+  public tasks:Task []= [
+    new Task ("washing clothes ",0),
+    new Task ("programming ",1),
+    new Task ("listening to music ",2),
+    new Task ("watching series",3),
+  ];
+}
+export class Task{
+  constructor(public description :string,public id :number){
+    
+  }
 }
